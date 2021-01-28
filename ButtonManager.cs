ac4083df58a9396a6a8012dd2f080114bfc5db0a
lumixem <78048342+lumixem@users.noreply.github.com>
2021-01-28 23:16:58 +0100

    Add files via upload

---
+++ ButtonManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void LoadPlayScene()
+    {
+        SceneManager.LoadScene(1);              
+    }
+   
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);              
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
