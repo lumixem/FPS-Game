e4ef32a9b9413f8996a085757637e1eccf6eab01
lumixem <78048342+lumixem@users.noreply.github.com>
2021-01-30 14:36:13 +0100

    Update and rename GrzybGenerator.cs to ShroomGenerator.cs

--- ShroomGenerator.cs
+++ ShroomGenerator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GrzybGenerator : MonoBehaviour
+public class ShroomGenerator : MonoBehaviour
 {
     public int enemyCount;
     float noSpawnRange = 2;
