eca0e5651dc574fe32a06562b35f5520b52258ee
lumixem <78048342+lumixem@users.noreply.github.com>
2021-02-01 18:20:03 +0100

    Update KamikazeAI.cs

--- KamikazeAI.cs
+++ KamikazeAI.cs
@@ -4,19 +4,10 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class KamikazeAI : MonoBehaviour
-{
-    public float kamikazeDamage = 34;   
-    public float DistanceFromPlayer = 50.0f;
-    public float time = 2.0f;
-    public float Distance;
-    public int kamikazeHP = 350;
-    
-    public AudioClip kamikazeExplosionSound;
+{    
     private NavMeshAgent agent;
     private GameObject player;
-    private Transform playerTransform;
     public GameObject kamikazeExplosion;
-    public GameObject kamikazeEnemy;
     Rigidbody rb;
     
     private Vector3 smoothVelocity = Vector3.zero;
@@ -29,7 +20,6 @@ public class KamikazeAI : MonoBehaviour
     void Start()
     {
         GetComponent<AudioSource>().playOnAwake = false;
-        GetComponent<AudioSource>().clip = kamikazeExplosionSound;
         GetComponent<NavMeshAgent>().speed = Random.Range(2f, 5f);
         agent.updateRotation = true;
         player = GameObject.FindGameObjectWithTag("Player");
