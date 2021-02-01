aee44253e6de240e9219ae7c7db1a564f6e62010
lumixem <78048342+lumixem@users.noreply.github.com>
2021-02-01 18:09:54 +0100

    Update KamikazeAI.cs

--- KamikazeAI.cs
+++ KamikazeAI.cs
@@ -10,8 +10,7 @@ public class KamikazeAI : MonoBehaviour
     public float time = 2.0f;
     public float Distance;
     public int kamikazeHP = 350;
-
-    //public AudioClip kamikazeWalking;
+    
     public AudioClip kamikazeExplosionSound;
     private NavMeshAgent agent;
     private GameObject player;
@@ -34,8 +33,7 @@ public class KamikazeAI : MonoBehaviour
         GetComponent<NavMeshAgent>().speed = Random.Range(2f, 5f);
         agent.updateRotation = true;
         player = GameObject.FindGameObjectWithTag("Player");
-        rb = GetComponent<Rigidbody>();
-        //playerTransform = GameObject.FindGameObjectWithTag("Player").transform;       
+        rb = GetComponent<Rigidbody>();               
     }
 
     void Update()
@@ -45,46 +43,13 @@ public class KamikazeAI : MonoBehaviour
             agent.SetDestination(player.transform.position);           
         }
     }
-
-    //void playAudio()
-    //{
-    //    AudioSource audio = GetComponent<AudioSource>();
-    //    if (!audio.isPlaying)
-    //    {
-    //        audio.clip = kamikazeExplosionSound;
-    //        audio.Play();
-    //    }
-    //}
-
-    //void FixedUpdate()
-    //{
-    //    transform.LookAt(playerTransform.transform);
-    //    float distancebetweenplayer = Vector3.Distance(transform.position, playerTransform.position);
-    //    if (distancebetweenplayer < DistanceFromPlayer)
-    //    {
-    //        //transform.position = Vector3.SmoothDamp(transform.position, player.position, ref smoothVelocity, time);
-    //        //if (rb.velocity.normalized != transform.forward.normalized)
-    //        //{
-    //        //    rb.AddForce(transform.forward * 5, ForceMode.Acceleration);
-    //        //}
-    //        //else
-    //        //{
-    //        //    rb.AddForce(transform.forward, ForceMode.Acceleration);
-    //        //}
-    //        transform.Translate(transform.forward * Time.deltaTime * 5);
-    //    }
-
-    //    Distance = Vector3.Distance(playerTransform.transform.position, transform.position);
-    //}
-
+    
     public void OnCollisionEnter(Collision collision)
     {       
         if (collision.gameObject.name == "Player")
         {
-            GetComponent<AudioSource>().Play();
-            //playAudio();
+            GetComponent<AudioSource>().Play();          
             Debug.Log("Kamikaze Collision");
-            //gameObject.SendMessage((isDamage)? "TakeDamage": "", SendMessageOptions.DontRequireReceiver);
             Destroy(this.gameObject);
             player.GetComponent<HP>().TakeDamage(34);           
             GameObject GObj = Instantiate(kamikazeExplosion);
